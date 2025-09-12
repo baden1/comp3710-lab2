9903f7948474bc51cbf358a5a8cb8d1d27cb9677
baden1 <badenforster@gmail.com>
2025-09-12 17:16:28 +1000

    evaluate average dice score of each segment

--- recognition/unet/unet_test.py
+++ recognition/unet/unet_test.py
@@ -1,6 +1,7 @@
 from unet_model import UNet
 from unet_utils import *
 import torch
+import numpy as np
 
 def dice_coeff(pred, target, epsilon=1e-6):
     """
@@ -10,7 +11,7 @@ def dice_coeff(pred, target, epsilon=1e-6):
     assert pred.shape == target.shape
     intersection = (pred * target).sum(dim=(1,2))   # sum over H,W per class
     union = pred.sum(dim=(1,2)) + target.sum(dim=(1,2))
-    dice = (2 * intersection + epsilon) / (union + epsilon)  # per class
+    dice = (2 * intersection) / (union + epsilon)  # per class
     return dice
 
 batch_size = 8
@@ -32,7 +33,9 @@ test_images_dataset = OasisDataset(
 test_masks_loader = DataLoader(test_masks_dataset, batch_size=batch_size, shuffle=False)
 test_images_loader = DataLoader(test_images_dataset, batch_size=batch_size, shuffle=False)
 
-dice_scores = []
+dice_scores = torch.zeros((len(test_masks_dataset), 4))
+
+
 for i, (masks, images) in enumerate(zip(test_masks_loader, test_images_loader)):
     # make mask from test image
 
@@ -40,11 +43,26 @@ for i, (masks, images) in enumerate(zip(test_masks_loader, test_images_loader)):
     probs = torch.softmax(outputs, dim=1)  # convert logits to probabilities
 
     # Convert predictions to one-hot: take argmax per pixel
-    preds = torch.argmax(probs, dim=1)      # [B,H,W], integer class
-    preds_onehot = torch.nn.functional.one_hot(preds, num_classes=4)  # [B,H,W,C]
-    preds_onehot = preds_onehot.permute(0,3,1,2).float()  # [B,C,H,W]
+    preds = torch.argmax(probs, dim=1)  # class labels
+    preds_onehot = torch.nn.functional.one_hot(preds, num_classes=4)
+    preds_onehot = preds_onehot.permute(0,3,1,2).float()  # re-order dimensions
+
+    # if i == 0:
+    #     for j in range(8):
+    #         pred_np = preds[j].cpu().numpy().astype(np.uint8)
+    #         colors = np.array([[0,0,0],[255,0,0],[0,255,0],[0,0,255]])  # example for 4 classes
+    #         color_img = colors[pred_np]  # [H,W,3]
+    #         img = Image.fromarray(color_img.astype(np.uint8))
+    #         img.save(f'H:\\comp3710\\comp3710-lab2\\recognition\\unet\\photos\\{j}.png')
+
 
     # Compute Dice per sample
-    for i in range(preds_onehot.size(0)):
-        dice = dice_coeff(preds_onehot[i], masks[i])
-        dice_scores.append(dice.item())
+    for j in range(preds_onehot.size(0)):
+        dice = dice_coeff(preds_onehot[j], masks[j])
+        dice_scores[batch_size * i + j] = dice
+
+
+print(f'Dice score of segment 0: {torch.mean(dice_scores[:,0])}')
+print(f'Dice score of segment 1: {torch.mean(dice_scores[:,1])}')
+print(f'Dice score of segment 2: {torch.mean(dice_scores[:,2])}')
+print(f'Dice score of segment 3: {torch.mean(dice_scores[:,3])}')
